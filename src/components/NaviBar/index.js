ce13456ece0214de27882b95c6373afa1f1b79e6
Taher Abouzeid <tabouzeid@gmail.com>
2020-09-28 23:04:51 -0400

    styling changes and placeholder for saved books

--- src/components/NaviBar/index.js
+++ src/components/NaviBar/index.js
@@ -2,17 +2,11 @@ import React from 'react'
 
 function NaviBar(){
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <nav className="nav justify-content-start">
-                        <span className="nav-link">Google Books Search</span>
-                        <a className="nav-link" href="/">Search</a>
-                        <a className="nav-link" href="/saved">Saved</a>
-                    </nav>
-                </div>
-            </div>
-        </div>
+        <nav className="navbar navbar-light bg-light justify-content-start">
+            <span className="nav-brand">Google Books Search</span>
+            <a className="nav-link" href="/">Search</a>
+            <a className="nav-link" href="/saved">Saved</a>
+        </nav>    
     );
 }
 
--- src/components/SavedPage/index.js
+++ src/components/SavedPage/index.js
@@ -3,7 +3,17 @@ import React from 'react'
 function SavedPage(){
     return (
         <div className="container">
-
+            <section className="row border border-secondary p-3">
+                <div className="col">
+                <div class="card">
+                    <div class="card-header">
+                        Saved
+                    </div>
+                    <div class="card-body">
+                    </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
