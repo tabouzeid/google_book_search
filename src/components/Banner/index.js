3a90b3165ff6d0d7542ddbf154e995ec7a07f0cd
Taher Abouzeid <tabouzeid@gmail.com>
2020-09-28 22:36:06 -0400

    added search form

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -4,7 +4,7 @@ function Banner(){
     return (
         <div className="container">
             <div className="row">
-                <div className="col border border-secondary">
+                <div className="col border border-secondary mb-5">
                     <h1 className="text-center">(React) Google Book Search</h1>
                     <h3 className="text-center">Search for and Save Books of Interest</h3>
                 </div>
--- src/components/SearchPage/index.js
+++ src/components/SearchPage/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function SearchPage(){
+    const [searchResults, setSearchResults] = useState({}); 
+    
     return (
         <div className="container">
+            <section className="row border border-secondary p-3">
+                <div className="col">
+                    <form>
+                        <div class="form-group">
+                            <label>Search</label>
+                            <input type="text" class="form-control" name="search-input" placeholder="Search by Book Name" aria-describedby="emailHelp"/>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Search</button>
+                    </form>
+                </div>
+            </section>
+            <section className="row">
+                <div className="col">
 
+                </div>
+            </section>
         </div>
     );
 }
