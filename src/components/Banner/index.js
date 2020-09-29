e06b98aeeb0a92000719e25f0ff740a328f7886b
Taher Abouzeid <tabouzeid@gmail.com>
2020-09-28 22:23:02 -0400

    finished banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,7 +3,12 @@ import React from 'react'
 function Banner(){
     return (
         <div className="container">
-
+            <div className="row">
+                <div className="col border border-secondary">
+                    <h1 className="text-center">(React) Google Book Search</h1>
+                    <h3 className="text-center">Search for and Save Books of Interest</h3>
+                </div>
+            </div>
         </div>
     );
 }
